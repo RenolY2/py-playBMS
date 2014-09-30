9c94c7c48f932e2134c2d520403fbfb09e464d95
Yoshi2 <debugyoshi@gmail.com>
2014-09-30 10:54:23 +0200

    Add Volume and Pitch methods

--- pygameMidi_extended.py
+++ pygameMidi_extended.py
@@ -3,8 +3,26 @@ from pygame.midi import Output
 
 
 class Output(Output):#pygame.midi.Output):
+    
     def set_pan(self, pan, channel):
         assert (0 <= channel <= 15)
         assert pan <= 127
         
         self.write_short(0xB0 + channel, 0x0A, pan)
+    
+    def set_volume(self, volume, channel):
+        assert (0 <= channel <= 15)
+        assert volume <= 127
+        
+        self.write_short(0xB0 + channel, 0x07, volume)
+        
+    def set_pitch(self, pitch, channel):
+        assert (0 <= channel <= 15)
+        assert pitch <= (2**14-1)
+        
+        # the 7 least significant bits come into the first data byte,
+        # the 7 most significant bits come into the second data byte
+        pitch_lsb = (pitch >> 7) & 127
+        pitch_msb = pitch & 127
+        
+        self.write_short(0xE0 + channel, pitch_lsb, pitch_msb)
