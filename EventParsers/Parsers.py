e2feee7a13bfede0eda2bd519dea645f8fdedc2b
Yoshi2 <debugyoshi@gmail.com>
2014-10-31 19:33:19 +0100

    Make code more compact

--- EventParsers/Parsers.py
+++ EventParsers/Parsers.py
@@ -14,7 +14,7 @@ baseParser.set_parser_function_range(   bin_struct("bb"),
 
 # Delay event for up to 255 ticks
 baseParser.set_parser_function( bin_struct("B"), 
-                                0x81)
+                                0x80)
 
 # Note-off event
 def parse_noteOff(bmsfile, read, strict, commandID):
@@ -31,7 +31,7 @@ baseParser.set_parser_function( bin_struct(">BBB"),
                                 0x9A)
 
 # Volume Change: Unknown, volume
-baseParser.set_parser_function( bin_struct(">BB"), 
+baseParser.set_parser_function( bin_struct(">BH"), 
                                 0x9C)
 
 # Pitch shift: Unknown, Pitch, Unknown
@@ -79,15 +79,15 @@ def parse_VL_delay(bmsfile, read, strict, commandID):
 baseParser.set_parser_function( parse_VL_delay, 
                                 0xF0)
 # BPM Value
-baseParser.set_parser_function(    bin_struct(">H"), 
+baseParser.set_parser_function( bin_struct(">H"), 
                                 0xFD)
 
 # PPQN Value
-baseParser.set_parser_function(    bin_struct(">H"), 
+baseParser.set_parser_function( bin_struct(">H"), 
                                 0xFE)
 
 # End of Track
-baseParser.set_parser_function(    bin_struct(""), 
+baseParser.set_parser_function( bin_struct(""), 
                                 0xFF)
 
 
@@ -97,97 +97,49 @@ container.add_parser(baseParser)
 Pik1_parser = VersionSpecificParser(0.5, "Pikmin 1")
 
 # Unknown Values
+
+
+Pik1_parser.set_many_parser_functions(bin_struct(">H"), 
+                                      0x94, 
+                                      0xA0, 0xA3, 0xA5, 
+                                      0xB3, 0xB4, 0xB8,
+                                      0xC9,
+                                      0xD0, 0xD1, 0xD5,
+                                      0xE6, 0xE7, 0xFA)
+
+Pik1_parser.set_many_parser_functions(bin_struct(">BB"),
+                                      0x98, 0xCB, 0xCC, 0xD2)
+
+Pik1_parser.set_many_parser_functions(bin_struct(">B"),
+                                      0xC2, 0xCD, 0xCF, 
+                                      0xDA, 0xDB, 0xDE,
+                                      0xF1)
+
+Pik1_parser.set_many_parser_functions(bin_struct(""),
+                                      0xA2, 0xA6, 0xA9,
+                                      0xB0, 0xBE,
+                                      0xC5, 0xCA, 
+                                      0xE1, 0xE3, 
+                                      0xF4)
+                                      
 Pik1_parser.set_parser_function(bin_struct(">HB"), 
                                 0x92)
-Pik1_parser.set_parser_function(bin_struct(">H"), 
-                                0x94)
-Pik1_parser.set_parser_function(bin_struct(">BB"), 
-                                0x98)
-
-Pik1_parser.set_parser_function(bin_struct(">H"), 
-                                0xA0)
-Pik1_parser.set_parser_function(bin_struct(""), 
-                                0xA2)
-Pik1_parser.set_parser_function(bin_struct(">H"), 
-                                0xA3)
-Pik1_parser.set_parser_function(bin_struct(">H"), 
-                                0xA5)
-Pik1_parser.set_parser_function(bin_struct(""), 
-                                0xA6)
-Pik1_parser.set_parser_function(bin_struct(""), 
-                                0xA9)
 Pik1_parser.set_parser_function(bin_struct(">I"), 
                                 0xAA)
 Pik1_parser.set_parser_function(bin_struct(">BBB"),
                                 0xAC)
 Pik1_parser.set_parser_function(bin_struct(">BBB"), 
                                 0xAF)
-Pik1_parser.set_parser_function(bin_struct(""), 
-                                0xB0)
-
-Pik1_parser.set_parser_function(bin_struct(">H"), 
-                                0xB3)
-Pik1_parser.set_parser_function(bin_struct(">H"), 
-                                0xB4)
-Pik1_parser.set_parser_function(bin_struct(">H"), 
-                                0xB8)
-Pik1_parser.set_parser_function(bin_struct(""), 
-                                0xBE)
-Pik1_parser.set_parser_function(bin_struct(">B"), 
-                                0xC2)
-Pik1_parser.set_parser_function(bin_struct(""), 
-                                0xC5)
 Pik1_parser.set_parser_function(bin_struct(">I"), 
                                 0xC7)
-Pik1_parser.set_parser_function(bin_struct(">H"), 
-                                0xC9)
-Pik1_parser.set_parser_function(bin_struct(""), 
-                                0xCA)
-Pik1_parser.set_parser_function(bin_struct(">BB"), 
-                                0xCB)
-Pik1_parser.set_parser_function(bin_struct(">BB"), 
-                                0xCC)
-Pik1_parser.set_parser_function(bin_struct(">B"), 
-                                0xCD)
-Pik1_parser.set_parser_function(bin_struct(">B"), 
-                                0xCF)
-Pik1_parser.set_parser_function(bin_struct(">H"), 
-                                0xD0)
-Pik1_parser.set_parser_function(bin_struct(">H"), 
-                                0xD1)
-Pik1_parser.set_parser_function(bin_struct(">BB"), 
-                                0xD2)
-Pik1_parser.set_parser_function(bin_struct(">H"), 
-                                0xD5)
-Pik1_parser.set_parser_function(bin_struct(">B"), 
-                                0xDA)
-Pik1_parser.set_parser_function(bin_struct(">B"), 
-                                0xDB)
 Pik1_parser.set_parser_function(bin_struct(">BH"), 
                                 0xDD)
-Pik1_parser.set_parser_function(bin_struct(">B"), 
-                                0xDE)
 Pik1_parser.set_parser_function(bin_struct(">I"), 
                                 0xDF)
 Pik1_parser.set_parser_function(bin_struct(">BH"), 
                                 0xE0)
-Pik1_parser.set_parser_function(bin_struct(""), 
-                                0xE1)
-Pik1_parser.set_parser_function(bin_struct(""), 
-                                0xE3)
-Pik1_parser.set_parser_function(bin_struct(">H"), 
-                                0xE6)
-Pik1_parser.set_parser_function(bin_struct(">H"), 
-                                0xE7)
 Pik1_parser.set_parser_function(bin_struct(">BBB"), 
                                 0xEB)
-Pik1_parser.set_parser_function(bin_struct(">B"), 
-                                0xF1)
-Pik1_parser.set_parser_function(bin_struct(""), 
-                                0xF4)
-Pik1_parser.set_parser_function(bin_struct(">H"), 
-                                0xFA)
-
 
 
 container.add_parser(Pik1_parser)
@@ -195,10 +147,21 @@ container.add_parser(Pik1_parser)
 # Super Mario Sunshine Parsing Stuff
 SMSunshine_parser = VersionSpecificParser(1, "Super Mario Sunshine")
 
+# Volume Change: Unknown, volume (volume is only 1 byte instead of two bytes)
+SMSunshine_parser.set_parser_function( bin_struct(">BB"), 
+                                0x9C)
+
 container.add_parser(SMSunshine_parser)
 
 # Zelda: Wind Waker Parsing Stuff
 Zelda_WW_parser = VersionSpecificParser(1.5, "Zelda: WW")
+
+# Volume Change: Unknown, volume (volume is 2 bytes again)
+# Maybe Nintendo thought that Mario did not need such a precision for SMS,
+# or maybe development of the audio engine for SMS was independent of the
+# development of the audio engine for Pikmin 1, which was released earlier.
+Zelda_WW_parser.set_parser_function( bin_struct(">BH"), 
+                                0x9C)
 
 container.add_parser(Zelda_WW_parser)
 
@@ -210,3 +173,5 @@ Pik2_parser.set_parser_function(bin_struct(">BH"),
                                 0x9C)
 
 container.add_parser(Pik2_parser)
+
+print "Done!"
