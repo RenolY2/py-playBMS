ff9dbadaa565c04b3c77903bb46b7ce0c4bd9c5f
Yoshi2 <debugyoshi@gmail.com>
2014-11-03 18:39:09 +0100

    Fix volume change reading one byte too much

--- EventParsers/SMarioSunshine_parser.py
+++ EventParsers/SMarioSunshine_parser.py
@@ -88,7 +88,6 @@ def parse_next_command(read, bmsfile, strict = True):
             elif controlEventID == 0x9C: # Volume Change
                 unknown1 = read.byte()
                 volume = read.byte()
-                unknown2 = read.byte()
                 
                 args = (unknown1, volume)
             
