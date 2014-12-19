dcd3688be35fb6ebaf5ea5dde306ee8b6f1b6649
Yoshi2 <debugyoshi@gmail.com>
2014-12-19 13:22:57 +0100

    Simple event module
    For executing functions on specific occasions

---
+++ bmsmodules/Events.py
@@ -0,0 +1,22 @@
+from operator import isCallable
+
+
+
+class Events(object):
+    def __init__(self):
+        self._events_ = {}
+    
+    def addEvent(self, eventname, func):
+        if not isCallable(func):
+            raise RuntimeError("func argument must be a function!")
+        elif not isinstance(eventname, basestring):
+            raise RuntimeError("Event name must be a string!")
+        elif eventname in self._events_:
+            raise RuntimeError("Event name already exists!")
+        
+        else:
+            self._events_[eventname] = func
+    
+    def execEvent(self, eventname, *args, **kwargs):
+        if eventname not in self._events_:
+            raise RuntimeError("No such Event name '{0}'".format(eventname))
